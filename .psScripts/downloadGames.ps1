6f49b8a5617b6e50cb8a928b6e25bb8e4aaf467b
DasDaOne <mr.ars355@GMAIL.COM>
2026-01-14 12:14:26 +0400

    Update downloadGames.ps1

--- .psScripts/downloadGames.ps1
+++ .psScripts/downloadGames.ps1
@@ -1,11 +1,9 @@
-$Url = "http://localhost:5185/api/watchdog/BotGames"
+$Url = "https://adt.carrothood.ru/api/watchdog/BotGames"
 $TargetDir = "E:\"
 $TargetFile = Join-Path $TargetDir "bt1.txt"
 
-# Создаем папку если нет
 if (!(Test-Path $TargetDir)) {
     New-Item -ItemType Directory -Path $TargetDir -Force | Out-Null
 }
 
-# Скачиваем с заменой
 Invoke-WebRequest -Uri $Url -OutFile $TargetFile -UseBasicParsing
