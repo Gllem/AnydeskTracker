a17e576d3659c1bc1e12173a92515e0a976e10a1
DasDaOne <mr.ars355@GMAIL.COM>
2026-01-21 19:52:49 +0400

    Update ApiAdminBotsController.cs

--- Controllers/Admin/AdminApi/Bots/ApiAdminBotsController.cs
+++ Controllers/Admin/AdminApi/Bots/ApiAdminBotsController.cs
@@ -78,7 +78,7 @@ public class ApiAdminBotsController(ApplicationDbContext dbContext) : Controller
 		return Ok();
 	}
 		
-	[HttpGet("actions/{date}")]
+	[HttpGet("{pcModelId}/actions/{date}")]
 	public async Task<IActionResult> GetBotActions(int pcModelId, string date)
 	{
 		DateTime logsDateTime;
